8f8023bcfc0d5f48c21bde32184d9cba8528e3ba
clautvald <53241046+clautvald@users.noreply.github.com>
2019-08-10 11:14:33 -0500

    Herencias de Figuras Geometricas

---
+++ Figura_Geometrica/src/FiguraGeometrica.java
@@ -0,0 +1,22 @@
+
+public abstract class FiguraGeometrica {
+	int medida_lado = 10;
+	int cantidad_lados = 4;
+
+	public FiguraGeometrica(int medida, int cantidad) {
+		medida_lado = medida;
+		cantidad_lados = cantidad;
+	}
+
+	public double ObtenerPerimetro() {
+		return medida_lado * cantidad_lados;
+	}
+
+	public abstract double ObtenerArea();
+	
+	public abstract void ImprimirFigura();
+		
+	
+	
+
+}
---
+++ Figura_Geometrica/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FiguraGeometrica cs = new Cuadrado(3);
+		
+		FiguraGeometrica c = new Triangulo(4);
+		
+		cs.ImprimirFigura();
+		c.ImprimirFigura();
+		
+		
+		
+	}
+
+}
